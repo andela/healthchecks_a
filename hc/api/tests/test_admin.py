48294687426b4a42c2dec9dc4e11fa6ca144b1a7
andela-ekaranja <edward.karanja@andela.com>
2016-09-22 16:15:20 +0300

    Adds functionality to test_admin.py

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -7,6 +7,9 @@ class ApiAdminTestCase(BaseTestCase):
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
+        self.alice.is_superuser = True
+        self.alice.is_staff = True
+        self.alice.save()
 
         ### Set Alice to be staff and superuser and save her :)
 
@@ -14,6 +17,9 @@ class ApiAdminTestCase(BaseTestCase):
         self.client.login(username="alice@example.org", password="password")
 
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
-        ch.save()
+        ch  .save()
 
         ### Assert for the push bullet
+        r = client.get("admin/api/channel/")
+        self.assertContains(r, "Pushbullet") #to revisit
+
