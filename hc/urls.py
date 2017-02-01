a833e830e3c84c4d4fb1632e6c47dcd93142dfdb
4bic <robyne.kiplangat@gmail.com>
2017-02-01 08:34:41 +0300

    added telegrambot

--- hc/urls.py
+++ hc/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('hc.accounts.urls')),
@@ -10,12 +11,12 @@ urlpatterns = [
     url(r'^telegrambot/', include('telegrambot.urls', namespace="telegrambot")),
 
 ]
-
-urlpatterns = [command('start', StartView.as_command_view()),
-           command('author', AuthorCommandView.as_command_view()),
-           command('author_inverse', AuthorInverseListView.as_command_view()),
-           command('author_query', login_required(AuthorCommandQueryView.as_command_view())),
-           unknown_command(UnknownView.as_command_view()),
-           regex(r'author_(?P<name>\w+)', AuthorName.as_command_view()),
-
-]
+# #telegrambot
+# urlpatterns = [command('start', StartView.as_command_view()),
+#            command('author', AuthorCommandView.as_command_view()),
+#            command('author_inverse', AuthorInverseListView.as_command_view()),
+#            command('author_query', login_required(AuthorCommandQueryView.as_command_view())),
+#            unknown_command(UnknownView.as_command_view()),
+#            regex(r'author_(?P<name>\w+)', AuthorName.as_command_view()),
+#
+# ]
