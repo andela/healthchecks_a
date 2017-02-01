532011e87d508263a004406aa72d784493116f32
4bic <robyne.kiplangat@gmail.com>
2017-02-01 08:29:32 +0300

    added telegrambot

--- hc/urls.py
+++ hc/urls.py
@@ -6,5 +6,16 @@ urlpatterns = [
     url(r'^accounts/', include('hc.accounts.urls')),
     url(r'^', include('hc.api.urls')),
     url(r'^', include('hc.front.urls')),
-    url(r'^', include('hc.payments.urls'))
+    url(r'^', include('hc.payments.urls')),
+    url(r'^telegrambot/', include('telegrambot.urls', namespace="telegrambot")),
+
+]
+
+urlpatterns = [command('start', StartView.as_command_view()),
+           command('author', AuthorCommandView.as_command_view()),
+           command('author_inverse', AuthorInverseListView.as_command_view()),
+           command('author_query', login_required(AuthorCommandQueryView.as_command_view())),
+           unknown_command(UnknownView.as_command_view()),
+           regex(r'author_(?P<name>\w+)', AuthorName.as_command_view()),
+
 ]
---
+++ hc_bot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HcBotConfig(AppConfig):
+    name = 'hc_bot'
